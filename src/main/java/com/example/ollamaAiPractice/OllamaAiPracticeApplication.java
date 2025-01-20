8e9686a8a913d171a96c279e90070196a6035fe1
jessicaccLemon123 <jessicacclemon@gmail.com>
2025-01-20 14:42:26 +0800

    test

--- src/main/java/com/example/ollamaAiPractice/OllamaAiPracticeApplication.java
+++ src/main/java/com/example/ollamaAiPractice/OllamaAiPracticeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OllamaAiPracticeApplication {
 
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(OllamaAiPracticeApplication.class, args);
 	}
